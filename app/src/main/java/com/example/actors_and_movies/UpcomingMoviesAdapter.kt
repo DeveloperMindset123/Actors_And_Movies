7b50778e09f89b1f4e7d597e42992c2383e832cf
DeveloperMindset123 <adas006@citymail.cuny.edu>
2024-03-27 12:07:18 -0400

    feat:updated changes with bug

---
+++ app/src/main/java/com/example/actors_and_movies/UpcomingMoviesAdapter.kt
@@ -0,0 +1,65 @@
+package com.codepath.articlesearch
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.actors_and_movies.R
+import com.example.actors_and_movies.UpcomingMovie
+//import com.example.actors_and_movies.DetailAc
+
+const val MOVIE_EXTRA = "MOVIE_EXTRA"
+
+class UpcomingMoviesAdapter(private val context: Context, private val movies: List<UpcomingMovie>) :
+    RecyclerView.Adapter<UpcomingMoviesAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.item_movies, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val movie = movies[position]
+        holder.bind(movie)
+    }
+
+    override fun getItemCount() = movies.size
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView), View.OnClickListener {
+        private val mediaImageView: ImageView = itemView.findViewById(R.id.mediaImage)
+        private val titleTextView: TextView = itemView.findViewById(R.id.mediaTitle)
+        private val overviewTextView: TextView = itemView.findViewById(R.id.mediaAbstract)
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        fun bind(movie: UpcomingMovie) {
+            titleTextView.text = movie.title
+            overviewTextView.text = movie.overview
+
+            val imagePath = "https://image.tmdb.org/t/p/w500/" + (movie.backdropPath ?: movie.posterPath)
+            Glide.with(context)
+                .load(imagePath)
+                .into(mediaImageView)
+        }
+
+        override fun onClick(v: View?) {
+            val movie = movies[absoluteAdapterPosition]
+
+            //Navigate to Details screen and pass selected article
+            val intent = Intent(context, DetailActivity::class.java)
+            intent.putExtra("MOVIE_EXTRA", movies[absoluteAdapterPosition]) //note that in order for this function to work, upcomingMovie needs to be set to java.io.serializable
+            context.startActivity(intent)
+/*
+            val intent = Intent(context, DetailActivity::class.java)
+            intent.putExtra(MOVIE_EXTRA, movie)
+            context.startActivity(intent) */
+        }
+    }
+}
