07fd17089ffaeb78bf280a2e22437eec7e845ab9
DeveloperMindset123 <adas006@citymail.cuny.edu>
2024-03-28 15:26:02 -0400

    feat:improved image rendering

--- app/src/main/java/com/example/actors_and_movies/DetailActivity.kt
+++ app/src/main/java/com/example/actors_and_movies/DetailActivity.kt
@@ -6,13 +6,16 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
-import com.example.actors_and_movies.R
-import com.example.actors_and_movies.UpcomingMovie
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions.withCrossFade
+import com.bumptech.glide.request.transition.DrawableCrossFadeFactory
+
 
 class DetailActivity : AppCompatActivity() {
     private lateinit var mediaImageView: ImageView
     private lateinit var titleTextView: TextView
     private lateinit var overviewTextView: TextView
+    //private lateinit var requestOptions : RequestOptions
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,8 +36,13 @@ class DetailActivity : AppCompatActivity() {
             "https://image.tmdb.org/t/p/w500/${upcomingMovie.posterPath}"
         }
 
+        val factory = DrawableCrossFadeFactory.Builder().setCrossFadeEnabled(true).build()
+        //requestOptions = requestOptions.transforms(new CenterCrop ())
         Glide.with(this)
             .load(imagePath)
+            .transition(withCrossFade(factory))
+            .transform(RoundedCorners(50))
             .into(mediaImageView)
+
     }
 }
